146a982acc7e3e36fd4a7bb8ed1295e4abcafcd9
siyuan <wsypeter@gmail.com>
2016-09-14 15:50:31 -0400

    Please enter the commit message for your changes. Lines starting get test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,3 +7,22 @@
  *
  * Main module of the application. Define the routing architecture for the app
  */
+
+
+var app = angular.module('workapp', ['ngRoute']);
+
+
+
+app.controller('workcontroller', function($scope, $http) {
+    $http({
+        method : "GET",
+        url : "/api/work"
+    }).then(function mySucces(response) {
+      //console.log('success');
+      console.log(response);
+        $scope.works = response.data.works;
+    }, function myError(response) {
+      console.log('error');
+        $scope.myWelcome = response.statusText;
+    });
+});
