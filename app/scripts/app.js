ed85d8eb5dba9c3fa282abc6e03495fdffc461ac
Isaac Feng <zfeng5@stevens.edu>
2016-09-15 10:39:34 -0400

    kevin route 2

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,20 +9,38 @@
  */
 
 
-var app = angular.module('workapp', ['ngRoute']);
+var app = angular.module('dashboard', ['ui.router']);
 
+app.config(function($stateProvider, $urlRouterProvider) {
+   $stateProvider
+   .state('login', {
+   		url:'/login',
+   		templateUrl: "templates/login.html"
+   	})
+   .state('root', {
+   		url:'/root',
+   		templateUrl: "templates/root.html"
+   	})
+   	.state('root.overview', {
+   		url:'/overview',
+   		templateUrl: "templates/overview.html"
+   	})
+   	.state('root.producer', {
+   		url:'/producer',
+   		templateUrl: "templates/producer.html"
+   	})
+   	.state('root.work', {
+   		url:'/work',
+   		templateUrl: "templates/work.html"
+   	})
 
+   	.state('root.contact', {
+   		url:'/contact',
+   		templateUrl: "templates/contact.html"
+   	});
 
-app.controller('workcontroller', function($scope, $http) {
-    $http({
-        method : "GET",
-        url : "/api/work"
-    }).then(function mySucces(response) {
-      //console.log('success');
-      console.log(response);
-        $scope.works = response.data.works;
-    }, function myError(response) {
-      console.log('error');
-        $scope.myWelcome = response.statusText;
-    });
+   	
+	$urlRouterProvider
+	.otherwise('/login');
 });
+
