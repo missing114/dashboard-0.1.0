8d689d882a9adb7fff1f0c1850ae8bffa34e719f
Isaac Feng <zfeng5@stevens.edu>
2016-09-16 14:21:46 -0400

    Kevin with dialog

--- app/scripts/directives/dialog.js
+++ app/scripts/directives/dialog.js
@@ -2,3 +2,18 @@
 // DIALOG DIRECTIVE
 // Description: Define 2 directives in the app: addDialog and editDialog. 
 // First one is for the "Add" button and the second one is for the "Edit" button
+
+
+angular.module('dashboard')
+.directive('addDialog', function(){
+	console.log("in directive");
+	return {
+		templateUrl: 'templates/addDialog.html'
+	};
+})
+.directive('editDialog', function(){
+	console.log("in directive");
+	return {
+		templateUrl: 'templates/editDialog.html'
+	};
+})
