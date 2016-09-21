7d18df402c9720c7440bd57d6ee2758d8681a891
missing114 <haotianpanzju@gmail.com>
2016-09-21 14:12:51 -0400

    all services implemented

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -2,3 +2,20 @@
 // CONTACT CONTROLLER
 // Description: Define the following functionalities:
 // All the functionalities on the "Contact" section for the form
+(function() {
+"use strict";
+
+angular.module('dashboard')
+.controller('ContactController', ContactController);
+console.log("ContactController is initialized");
+
+ContactController.$inject = ['$scope', 'ContactService'];
+function ContactController($scope, ContactService) {
+	$scope.username = '';
+  	$scope.usernickname = '';
+  	$scope.userimg = '';
+
+	
+};
+
+})();
--- app/scripts/services/getContact.js
+++ app/scripts/services/getContact.js
@@ -1,3 +1,33 @@
 'use strict';
 // GETCONTACT SERVICE
 // Description: Define the contactService that has 2 functionalities: getLocation and getCategory 
+
+(function() {
+"use strict";
+
+angular.module('dashboard').service('ContactService', ContactService);
+console.log("ContactService is initialized");
+ContactService.$inject = ['$http', '$window'];
+function ContactService($http, $window) {
+  var s = this;
+
+  s.getLocation = function() {
+    var params = {};
+
+    return $http.get('/api/location', params).then(function(response) {
+        return response.data.location;
+  	});
+  };
+
+  s.getCategory= function() {
+    var params = {};
+
+    return $http.get('/api/category', params).then(function(response) {
+        return response.data.category;
+    });
+  };
+
+  // return this;
+}
+
+})();
