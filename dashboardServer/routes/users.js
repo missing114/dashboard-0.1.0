5f076d8601c94275989ee66e998fb03974d4a475
missing114 <haotianpanzju@gmail.com>
2016-09-22 20:00:08 -0400

    contact draft

--- dashboardServer/routes/users.js
+++ dashboardServer/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router  = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+	console.log(req.query);
     if (req.query.user === 'minh') {
         res.send({
             name: 'Minh Pham'
